05e5b45859f97fccba216a11001ce65c5eb2ff84
satishbonam <satishb@autoinfer.com>
2020-11-26 08:01:35 +0530

    Added Dockerfile dev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -13,10 +13,8 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noopener noreferrer"></a>
+        <div>hey satish</div>
       </header>
     </div>
   );
